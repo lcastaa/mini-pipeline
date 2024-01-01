2026b279fda62776214882ec4e69c428f555b201
Luis Castaneda <108830053+lcastaa@users.noreply.github.com>
2023-12-31 17:36:41 -0800

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def github_webhook():
         return jsonify({'message': 'Received Push Event !! Now Executing Pipeline...'}), 200
 
     else:
-        return jsonify({'message': 'Unknown GitHub event !! Error: No action has been created to handle GitHub Event: ' + request.headers.get('X-GitHub-Event')}), 400
+        return jsonify({'message': 'Unknown GitHub event !! Error: No action has been created to handle GitHub Event: ' + request.headers.>
 
 
 # This is the actual Pipeline You can add stages in the core_methods
@@ -33,11 +33,9 @@ def pipeline(github_repo):
         core_methods.stage_six(config_data)
     else:
         core_methods.stage_six_with_secrets(config_data)
-    core_methods.clean_up()
+    core_methods.clean_up(cloned_repo)
 
 
 if __name__ == '__main__':
 
     app.run(host='0.0.0.0', port=3000, debug=False, use_reloader=False)
-
-
